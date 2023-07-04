4b53e6078907c11dad9d8438482f523b0356e120
GabrielVitorSilva <gabrieldatas2004@gmail.com>
2023-07-04 14:26:59 -0300

    feat: added tab navigator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { Loading } from "@components/loading";
-import { NativeBaseProvider } from "native-base";
-import { THEME } from "./src/theme";
+
 import { Routes } from "@routes/index";
+
+import { THEME } from "./src/theme";
+
+import { Loading } from "@components/loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,8 +18,8 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={"transparent"}
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
       />
       {fontsLoaded ? <Routes /> : <Loading />}
