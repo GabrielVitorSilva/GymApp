19948e2199b1429bdfdfb11d9388c2636313c544
GabrielVitorSilva <gabrieldatas2004@gmail.com>
2023-07-03 23:09:33 -0300

    Feat: added stack navigator and authContextNavigator

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { Loading } from "@components/loading";
 import { NativeBaseProvider } from "native-base";
 import { THEME } from "./src/theme";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,38 +1,48 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
-import Logo from "@assets/logo.svg";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
 export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg={"gray.700"} px={10}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
-          alt="People trainnig"
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
           resizeMode="contain"
-          position={"absolute"}
-        ></Image>
+          position="absolute"
+        />
+
         <Center my={24}>
-          <Logo />
-          <Text color={"gray.100"} fontSize={"sm"}>
-            Treine sua mente e seu corpo
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
           </Text>
         </Center>
+
         <Center>
-          <Heading
-            color={"gray.100"}
-            fontSize={"xl"}
-            mb={6}
-            fontFamily={"heading"}
-          >
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
 
           <Input placeholder="Nome" />
+
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
@@ -41,8 +51,14 @@ export function SignUp() {
           <Input placeholder="Senha" secureTextEntry />
 
           <Button title="Criar e acessar" />
-          <Button title="Voltar para login" variant={"outline"} mt={24} />
         </Center>
+
+        <Button
+          title="Voltar para o login"
+          variant="outline"
+          mt={24}
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
   );
